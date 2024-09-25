f4bd915c81b5fdc7024c0c1d9fcd840dec86b82f
shaheryarbhatti456 <subtakeit@gmail.com>
2024-09-25 15:26:22 +0500

    changes in vender folder

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit7e8c3c14ff33b199b4a0838993eb8423::getLoader();
+return ComposerAutoloaderInit7689e75ffeff3a4bd99c5fce9c6f828c::getLoader();
--- vendor/laravel/framework/src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
@@ -28,6 +28,13 @@ class ApplicationBuilder
     protected array $pendingProviders = [];
 
     /**
+     * Any additional routing callbacks that should be invoked while registering routes.
+     *
+     * @var array
+     */
+    protected array $additionalRoutingCallbacks = [];
+
+    /**
      * The Folio / page middleware that have been defined by the user.
      *
      * @var array
@@ -222,6 +229,10 @@ class ApplicationBuilder
                 }
             }
 
+            foreach ($this->additionalRoutingCallbacks as $callback) {
+                $callback();
+            }
+
             if (is_string($pages) &&
                 realpath($pages) !== false &&
                 class_exists(Folio::class)) {
@@ -272,7 +283,13 @@ class ApplicationBuilder
     public function withCommands(array $commands = [])
     {
         if (empty($commands)) {
-            $commands = [$this->app->path('Console/Commands')];
+            if (is_file($this->app->basePath('routes/console.php'))) {
+                $commands = [$this->app->basePath('routes/console.php')];
+            }
+
+            if (is_dir($this->app->path('Console/Commands'))) {
+                $commands = [...$commands, $this->app->path('Console/Commands')];
+            }
         }
 
         $this->app->afterResolving(ConsoleKernel::class, function ($kernel) use ($commands) {
